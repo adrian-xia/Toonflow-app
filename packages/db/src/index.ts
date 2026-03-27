b243322de06a04a8b3643942604e5acfa1f2c201
Adrian Xia <adrian_xia@163.com>
2026-03-27 08:57:07 +0800

    fix: narrow db public api

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,7 +1,6 @@
 export { DbConfig, DbPoolConfig } from "./config/db-config";
 export { readDbConfig, ReadDbConfigOptions } from "./config/read-db-config";
 export { DbClient, DbExecutor } from "./client/types";
-export { buildKnexConfig } from "./client/knex-config";
 export { createDbClient } from "./client/create-db-client";
 export {
   createProjectRepository,
